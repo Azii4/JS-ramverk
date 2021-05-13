eb1c09de9c8138a51b4b7e6ed41da080cb1def25
Josefine Klintberg <jool@snel.se>
2021-05-13 15:59:26 +0200

    Marked positition of star and delete.

--- src/Movie.js
+++ src/Movie.js
@@ -5,14 +5,15 @@ import starIcon from "./images/star.png";
 const getStars = (rating) => {
   return Array(rating)
     .fill(0)
-    .map((_, i) => <img key={i} src={starIcon} alt="star" />);
+    .map((_, i) => (
+      <img key={i} src={starIcon} alt="star" className="float-end" />
+    ));
 };
 
 export default function Movie(props) {
   return (
     <li>
       {props.item.title}
-      {getStars(props.item.grade)}
       <img
         src={deleteIcon}
         alt="X"
@@ -21,6 +22,7 @@ export default function Movie(props) {
           props.deleteItem(props.item.id);
         }}
       ></img>
+      {getStars(props.item.grade)}
     </li>
   );
 }
