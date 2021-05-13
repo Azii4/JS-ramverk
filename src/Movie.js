0af2aaeb766450df6aa0dd8dbc05e5d78e97cf04
Azii <Mattias.johnsond@hotmail.com>
2021-05-13 14:52:36 +0200

    uppdate movie

--- src/Movie.js
+++ src/Movie.js
@@ -5,19 +5,10 @@ import starIcon from "./images/star.png";
 export default function Movie(props) {
   console.log(props.item.grade);
 
-  function getStars(rating) {
-      i = 
-    for (let i = 0; range({props.item.grade})){
-
-    }
-    return (<img src={starIcon} alt="star" />).repeat(rating);
-  }
-
   return (
     <li>
       {props.item.title}
       {props.item.grade}
-      {getStars(props.item.grade)}
       <img
         src={deleteIcon}
         alt="X"
